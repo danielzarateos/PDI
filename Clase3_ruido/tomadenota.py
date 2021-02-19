55b36fdc4eba60687f537d52779a11fc1e9dc313
Daniel <danielzarate317@gmail.com>
2021-02-19 08:28:41 -0500

    g

---
+++ Clase3_ruido/tomadenota.py
@@ -0,0 +1,22 @@
+#Ruido, que es y la variabilidad en sistemas de procesamiento
+"Todo aquello indeseado en l captura y el procesamiento de bioseñales, el ruido"
+"es una fuente de variabilidad"
+
+"En las bioseñales hay varias fuentes"
+"Fisiologicas"
+"Ambientales"
+"Del artefacto"
+"Ruido electronico"
+
+"EL RUIDO ELECTRONICO"
+"Es el unico que se puede describir completamente"
+
+"EL RUIDO TERMICO"
+"Producido por resistivos y depende de temperaturas y ancho de banda"
+
+#Relacion señal a ruido
+"Magnitud de la señal con respecto al ruido medidos en valor RMS o root mean square"
+#SNR= 20LOG((Xseñal)/(Xruido)) [dB]
+"X = voltaje o corriente"
+
+#lineal SNRlineal = 10^((SNRdb)/(20))

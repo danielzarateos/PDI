94b9fb6c335d315e506423ebdf0f50d6c0dae487
Daniel <danielzarate317@gmail.com>
2021-02-19 09:55:10 -0500

    h

--- Clase4_masruido/nota.py
+++ Clase4_masruido/nota.py
@@ -1,2 +1,26 @@
 #El ruido afecta todo y hay ruidos distintos con distintas distribuciones,
 #distribucion gaussiana del ruido 
+#Medidas estadisticas basicas 
+
+#Funciones y transformaciones
+#Generan mejoras en calidad, cambios o facilidades para interpretarlos o reducirlos
+#Remapeo
+#Vamos a ver correrlacion y convulcion (correlacion cruzada pero al revez)
+
+
+#La correlacoin matematica es la multiplicacion punto a punto de dos funciones
+
+#Las funciones a comparar deben tener offset cero
+
+#Cuando no se parecen la correlacion tiende a cero
+#Magnitudes grandes negativas = opuestas
+#Magnitudes grandes positivas = similares
+
+#Truncar es recortar
+
+#Autocorrelacion (que tanto me parezco a mi, para evaluar
+# periodicidad) y correlacion cruzada 
+
+#La convolucion no corre de derecha a izq sino de izq a derecha
+
+#Correlacion de Pearson

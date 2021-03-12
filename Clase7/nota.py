eee707bcd964d2a44571c83431e2269e465a3736
Daniel <danielzarate317@gmail.com>
2021-03-12 09:27:05 -0500

    l

---
+++ Clase7/nota.py
@@ -0,0 +1,5 @@
+#Fourier, F1 es el primer dato
+#Hay que capturar cierto tiempo y F1 = 1/t o periodo de la señal
+#sum x(n) cos o sin (2 pi nf N Ts)
+# acos(x) + bsin(x) 0 Ccos(x - ª)
+#C = (a^2 + b^2)^(1/2)

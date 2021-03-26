eab1294e77fc401228ec29786e8f23f770299282
Daniel <danielzarate317@gmail.com>
2021-03-26 08:01:03 -0500

    Hola

---
+++ TallerPreParcial/taller.py
@@ -0,0 +1,22 @@
+#1 8hz
+#2 20Hz
+#3 0.3Hz
+#4 0.5Hz
+#5 0.5Hz
+#6 8Hz
+#7 8Hz
+
+#8 SNRdb = 20 log ( X señal rms / X ruido rms)
+#  SNRdb = 20 log ( 2.5V/0.028)
+#  SNRdb = 39.016dB
+
+#9 SNRdb = 20 log ( X señal rms / X ruido rms)
+#  SNRdb / 20 = log ( X señal rms / X ruido rms)
+#  10 ^ SNRdb / 20 = ( X señal rms / X ruido rms )
+#  X señal rms = X ruido rms * 10 ^ SNRdb / 20 
+#  X señal rms = 0.5V * 10 ^10/20
+#  X señal rms = 1.58V
+
+#12. 125datos / segundo * 60 = 7500 datos/min
+#    4500 datos = 10 respiraciones
+#    7500 datos > X respiraciones
